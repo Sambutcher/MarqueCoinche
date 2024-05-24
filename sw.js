a8868bf6af55b4ccd5a813d5d25a44cb20ff3423
Sambutcher <Samuel.boucher@gmail.com>
2024-05-24 13:27:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "cache120240524v2"; // Change value to force update
+const cacheName = "cache120240524v3"; // Change value to force update
 
 self.addEventListener("install", event => {
 	// Kick out the old service worker
